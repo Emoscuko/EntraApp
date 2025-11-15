01c3cb69a1278ed662edade8bec96350c55e139d
Emoscuko <165709356+Emoscuko@users.noreply.github.com>
2025-11-15 19:29:23 +0300

    aa

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+// src/main.jsx
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app.jsx";
+import "./index.css";
+
+// MSAL için importlar
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./authConfig"; // Yapılandırmamızı import et
+
+// MSAL örneğini (instance) oluştur
+const msalInstance = new PublicClientApplication(msalConfig);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* Tüm App bileşenini MsalProvider ile sarmala */}
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>
+);
